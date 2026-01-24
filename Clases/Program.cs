735f3877cf1a9195c1597853d17bc739c69be07c
JoelSoliz <joelsolizchoque@gmail.com>
2026-01-24 12:43:46 -0400

    add content class 1

---
+++ Clases/Program.cs
@@ -0,0 +1,50 @@
+﻿//int age = 20;
+//long num = 10000000000000000;
+//double height = 1.5;
+//decimal price = 2.5m;
+//bool isOpen = true;
+//char letter = 'a';
+
+//string name = "Luciana";
+//name += " Flores";
+
+//num = age;
+//age = (int)num;
+//height = age;
+//age = (int)height;
+
+//int? age2 = null;
+//int sum = age + (int)age2;
+
+//int age3 = int.Parse("1a");
+
+//bool parsed = int.TryParse("1a", out int age4);
+
+//var sum2 = 1m;
+
+//object obj = "string";
+//int objn = (int)obj;
+
+//dynamic dynob = "strring";
+//dynob.ToLower();
+
+//person?.age != null ? person.age : 0;
+
+
+//void RegularIncrement(int x)
+//{
+//    x++;
+//}
+
+//void Increment(ref int x)
+//{
+//    x++;
+//}
+
+//int a = 5;
+//RegularIncrement(a);
+//Console.WriteLine(a);
+
+//Increment(ref a);
+//Console.WriteLine(a);
+
---
+++ Ejercicios/Program.cs
@@ -0,0 +1,104 @@
+﻿#region JoaoJoao
+//int JoaoJoao(int[] difficultyLevels)
+//{
+//    //bool level1 = false;
+//    //bool level2 = false;
+//    //bool level3 = false;
+//    HashSet<int> validatedDifficulties = new HashSet<int>() { 1,2,3,4 };
+//    //bool level4 = false;
+//    for (int i = 0; i < difficultyLevels.Length; i++)
+//    {
+//        if (validatedDifficulties.Contains(difficultyLevels[i]))
+//        {
+//            validatedDifficulties.Remove(difficultyLevels[i]);
+//        }
+//        //    if (difficultyLevels[i] == 1)
+//        //    {
+//        //        level1 = true;
+//        //    }
+
+//        //    if (difficultyLevels[i] == 2)
+//        //    {
+//        //        level2 = true;
+//        //    }
+
+//        //    if (difficultyLevels[i] == 3)
+//        //    {
+//        //        level3 = true;
+//        //    }
+
+//        //    if (difficultyLevels[i] == 4)
+//        //    {
+//        //        level4 = true;
+//        //    }
+//    }
+
+//    //HashSet<int> validatedDifficulties = new HashSet<int>(difficultyLevels);
+
+//    //return 4 - (level1 ? 1 : 0) - (level2 ? 1 : 0) - (level3 ? 1 : 0) - (level4 ? 1 : 0);
+//    //return 4 - validatedDifficulties.Count;
+//    return validatedDifficulties.Count;
+//}
+
+
+//int[] difficultyLevels1 = new int[] { 1, 3, 4, 1, 3, 4, 1, 3, 4, 1 };
+//Console.WriteLine(JoaoJoao(difficultyLevels1));
+
+//int[] difficultyLevels2 = new int[] { 4, 1, 1, 4, 3, 1, 2, 1, 2, 2 };
+//Console.WriteLine(JoaoJoao(difficultyLevels2));
+#endregion
+
+#region AttentionMeeting
+//int AttentionToTheMeeting(int n, int k)
+//{
+//    int speachInterval = 1;
+//    int intervals = n - 1;
+//    int remainingTime = k - intervals * speachInterval;
+//    return remainingTime / n;
+//}
+
+
+//int N = 7; // N Directors
+//int K = 120; // Max Meeting Duration
+//Console.WriteLine(AttentionToTheMeeting(N, K));
+
+//N = 1; // N Directors
+//K = 10; // Max Meeting Duration
+//Console.WriteLine(AttentionToTheMeeting(N, K));
+
+//N = 100; // N Directors
+//K = 1000; // Max Meeting Duration
+//Console.WriteLine(AttentionToTheMeeting(N, K));
+#endregion
+
+#region AppendPanic
+int AppendAndPanic(string input)
+{
+    //HashSet<char> letters = new HashSet<char>(input);
+    //// |input| = |original| + |unicos|
+    //// |original| = |input| - |unicos|
+    //return input.Length - letters.Count;
+    HashSet<char> letters = new HashSet<char>();
+    for(int i = input.Length - 1; i >= input.Length / 2 - 1; i--)
+    {
+        if (!letters.Contains(input[i]))
+        {
+            letters.Add(input[i]);
+        }
+        else
+        {
+            return i + 1;
+        }
+    }
+
+    return 0;
+}
+
+
+string S = "ICPCCIP"; // input
+Console.WriteLine(AppendAndPanic(S));
+string S2 = "ABEDCCCABCDE";
+Console.WriteLine(AppendAndPanic(S2));
+string S3 = "ZZ";
+Console.WriteLine(AppendAndPanic(S3));
+#endregion
