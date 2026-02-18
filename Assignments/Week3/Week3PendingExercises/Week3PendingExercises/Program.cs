617b83afb9731541bad1ba412a5c765a53d903d5
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2026-02-17 21:35:16 -0400

    Add solutions to exercises given in class

---
+++ Assignments/Week3/Week3PendingExercises/Week3PendingExercises/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        /* Healthy Menu
+        HealthyMenu.CalculateStudents();
+        */
+
+        LexicographicalAnalysis.AnalyzeBytes();
+
+
+    }
+
+}
