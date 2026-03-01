948303c129347b9ae367f326d8c974fc454a144a
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2026-02-28 22:29:45 -0400

    Add solutions of 4 to 7 exercises

---
+++ Classes/ExercisesWeek4/Exercise5.cs
@@ -0,0 +1,104 @@
+﻿using Classes.Class3;
+
+namespace Classes.ExercisesWeek4
+{
+    public class DataAggregator
+    {
+        public async Task<List<Song>> GetFromDatabaseAsync()
+        {
+            await Task.Delay(400);
+            var songs = TestData.GetSongs();
+
+            var databaseSongs = songs.Where(song => song.PlayCount >= 450_000_000).ToList();
+            return databaseSongs;
+        }
+
+        public async Task<List<Song>> GetFromCacheAsync()
+        {
+            await Task.Delay(50);
+            var songs = new List<Song>();
+            return songs;
+        }
+
+        public async Task<List<Song>> GetFromApiAsync()
+        {
+            await Task.Delay(800);
+            var songs = TestData.GetSongs();
+            return songs;
+        }
+
+        public async Task<List<Song>> GetFastestAsync()
+        {
+            List<Task<List<Song>>> tasks = new List<Task<List<Song>>> {
+                GetFromDatabaseAsync(),
+                GetFromCacheAsync(),
+                GetFromApiAsync()
+            };
+
+            while (tasks.Count > 0)
+            {
+                var fastestProcess = await Task.WhenAny(tasks);
+                var result = await fastestProcess;
+                if (result.Count > 0)
+                {
+                    return result;
+                }
+                tasks.Remove(fastestProcess);
+            }
+            return new List<Song>(); // if all are empty
+        }
+
+        public async Task<List<Song>> GetFastestProcessTimeout()
+        {
+            var delay = Task.Delay(600);
+            List<Task<List<Song>>> tasks = new List<Task<List<Song>>> {
+                GetFromDatabaseAsync(),
+                GetFromCacheAsync(),
+                GetFromApiAsync(),
+            };
+
+            while (tasks.Count > 0)
+            {
+                var fastestProcess = await Task.WhenAny(tasks.Cast<Task>().Append(delay));
+                if (fastestProcess == delay)
+                {
+                    throw new TimeoutException();
+                }
+                var dataTask = (Task<List<Song>>)fastestProcess;
+                var result = await dataTask;
+
+                if (result.Count > 0) return result;
+                tasks.Remove(dataTask);
+            }
+            return new List<Song>();
+        }
+    }
+
+    /*
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var dataAggregator = new DataAggregator();
+            var result = await dataAggregator.GetFastestAsync();
+            foreach (var song in result)
+            {
+                Console.WriteLine(song);
+            }
+
+            try
+            {
+                var fastestProcess = await dataAggregator.GetFastestProcessTimeout();
+                foreach (var song in fastestProcess)
+                {
+                    Console.WriteLine(song);
+                }
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("Any process has respond on an adequate time");
+            }
+        }
+    }
+    */
+}
---
+++ Classes/ExercisesWeek4/Exercise6.cs
@@ -0,0 +1,97 @@
+﻿namespace Classes.ExercisesWeek4
+{
+    public class ApiException : Exception
+    {
+        public int StatusCode { get; }
+        public ApiException(string message) : base(message) { }
+
+        public ApiException(int statusCode, string message, Exception innerException) : base(message, innerException) { StatusCode = statusCode; }
+
+        public ApiException(int statusCode, string message) : base(message) { StatusCode = statusCode; }
+    }
+
+    public class DatabaseException : ApiException
+    {
+        public DatabaseException(string message, Exception inner) : base(500, "We couldn't process your request. Please try again", inner) { }
+    }
+
+    public class NotFoundException : ApiException
+    {
+        public NotFoundException(string message) : base(404, "We couldn't found the requested resource. Please, try with a valid one") { }
+    }
+
+    public class ValidationException : ApiException
+    {
+        public ValidationException(string message) : base(400, "Bad request. Please try a valid operation") { }
+    }
+
+    public class ApiResponse
+    {
+        public int StatusCode { get; init; }
+        public string Message { get; init; } = "";
+        public string TraceId { get; init; } = "";
+    }
+
+    public class RequestHandler
+    {
+        public async Task<ApiResponse> HandleRequestAsync(Func<Task> handler)
+        {
+            try
+            {
+                await handler();
+                var response = new ApiResponse
+                {
+                    StatusCode = 200,
+                    Message = "Successfully request",
+                    TraceId = Guid.NewGuid().ToString()
+                };
+                return response;
+            }
+            catch (ApiException apiException)
+            {
+                var response = new ApiResponse
+                {
+                    StatusCode = apiException.StatusCode,
+                    Message = apiException.Message,
+                    TraceId = Guid.NewGuid().ToString()
+                };
+                return response;
+            }
+            catch (Exception unexpectedException)
+            {
+                Console.WriteLine($"Unexcpected Exception: {unexpectedException.InnerException}");
+                var response = new ApiResponse
+                {
+                    StatusCode = 500,
+                    Message = $"Unexcpected Exception: {unexpectedException.InnerException}",
+                    TraceId = Guid.NewGuid().ToString()
+                };
+                return response;
+            }
+        }
+    }
+    /*
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var sqlException = new Exception("SQL connection timeout");
+            var databaseException = new DatabaseException("Sql Exception found!", sqlException);
+
+            var handler = new RequestHandler();
+            var response = await handler.HandleRequestAsync(async () =>
+                           {
+                               throw databaseException;
+                           });
+            Console.WriteLine($"StatusCode: {response.StatusCode}, Message: {response.Message}, TraceId: {response.TraceId}");
+
+            Exception? currentException = databaseException; //it can be null
+            while (currentException != null)
+            {
+                Console.WriteLine($"{currentException.Message}");
+                currentException = currentException.InnerException; //iterate until null
+            }
+        }
+    }
+    */
+}
