c6a7608062e765d2f76e5380231b6903aff091f3
lyandcat13004 <flores.torrico.lucianae@gmail.com>
2026-04-01 21:17:11 -0400

    Fix moderator user data seeder creation

--- Assignments/Week5/WatchTrackerAPI/Migrations/20260402011545_ChangeRoleColumnToString.Designer.cs
+++ Assignments/Week5/WatchTrackerAPI/Migrations/20260402011545_ChangeRoleColumnToString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WatchTrackerAPI.Data;
 
@@ -11,9 +12,11 @@ using WatchTrackerAPI.Data;
 namespace WatchTrackerAPI.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260402011545_ChangeRoleColumnToString")]
+    partial class ChangeRoleColumnToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
